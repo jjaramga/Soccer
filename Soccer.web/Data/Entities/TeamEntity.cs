8f7d0fbc0d5f679df360ba455ca66d019de75dd5
JUAN FELIPE JARAMILLO GALEANO <juanjaramillo223677@correo.itm.edu.co>
2020-02-16 19:39:37 -0500

    Add Database

---
+++ Soccer.web/Data/Entities/TeamEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Soccer.web.Data.Entities
+{
+    public class TeamEntity
+    {
+        public int Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "The field {Name} can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {Name} is mandatory.")]
+        public string Name { get; set; }
+
+        public string LogoPath { get; set; }
+    }
+}
--- Soccer.web/Startup.cs
+++ Soccer.web/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Soccer.web.Data;
 
 namespace Soccer.web
 {
@@ -21,15 +23,21 @@ namespace Soccer.web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
+            services.AddDbContext<DataContext>(cfg =>
+            {
+                cfg.UseSqlServer(Configuration.GetConnectionString("SoccerConnection"));
+            });
+
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
