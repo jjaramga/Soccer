65dc3d9cdcc3177b8c758c860fa896422a52a7e1
JUAN FELIPE JARAMILLO GALEANO <juanjaramillo223677@correo.itm.edu.co>
2020-03-01 21:32:43 -0500

    Implementation SeedDB

--- Soccer.web/Startup.cs
+++ Soccer.web/Startup.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -25,12 +20,12 @@ namespace Soccer.web
 
         public IConfiguration Configuration { get; }
 
-        
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                
+
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
@@ -40,7 +35,7 @@ namespace Soccer.web
                 cfg.UseSqlServer(Configuration.GetConnectionString("SoccerConnection"));
             });
 
-
+            services.AddTransient<SeedDb>();
             services.AddScoped<IImageHelper, ImageHelper>();
             services.AddScoped<IConverterHelper, ConverterHelper>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
