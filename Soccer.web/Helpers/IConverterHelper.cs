8b3a4e11e066716b0d8453f679c35c6e8a3defb1
JUAN FELIPE JARAMILLO GALEANO <juanjaramillo223677@correo.itm.edu.co>
2020-03-02 21:59:28 -0500

    Improve TournamentController (GroupDetails)

--- Soccer.web/Helpers/IConverterHelper.cs
+++ Soccer.web/Helpers/IConverterHelper.cs
@@ -1,7 +1,8 @@
 ﻿using Soccer.web.Data.Entities;
 using Soccer.web.Models;
 using Soccer.Web.Data.Entities;
-
+using Soccer.Web.Models;
+using System.Threading.Tasks;
 
 namespace Soccer.Web.Helpers
 {
@@ -10,5 +11,15 @@ namespace Soccer.Web.Helpers
         TeamEntity ToTeamEntity(TeamViewModel model, string path, bool isNew);
 
         TeamViewModel ToTeamViewModel(TeamEntity teamEntity);
+
+        TournamentEntity ToTournamentEntity(TournamentViewModel model, string path, bool isNew);
+
+        TournamentViewModel ToTournamentViewModel(TournamentEntity tournamentEntity);
+
+        Task<GroupEntity> ToGroupEntityAsync(GroupViewModel model, bool isNew);
+
+        GroupViewModel ToGroupViewModel(GroupEntity groupEntity);
+
+
     }
 }
