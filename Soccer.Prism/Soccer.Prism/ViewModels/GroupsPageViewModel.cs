19b61d140d316c493275100d781668079e21b821
JUAN FELIPE JARAMILLO GALEANO <juanjaramillo223677@correo.itm.edu.co>
2020-03-22 15:54:06 -0500

    Navigation another page

---
+++ Soccer.Prism/Soccer.Prism/ViewModels/GroupsPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Soccer.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Soccer.Prism.ViewModels
+{
+    public class GroupsPageViewModel : ViewModelBase
+    {
+        private TournamentResponse _tournament;
+        public GroupsPageViewModel(INavigationService navigationService) :base (navigationService)
+        {
+            Title = "Groups";
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+
+            _tournament = parameters.GetValue<TournamentResponse>("tournament");
+            Title = _tournament.Name;
+        }
+    }
+}
---
+++ Soccer.Prism/Soccer.Prism/ViewModels/TournamentItemViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Prism.Commands;
+using Prism.Navigation;
+using Soccer.Common.Models;
+using Soccer.Prism.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Soccer.Prism.ViewModels
+{
+    public class TournamentItemViewModel:TournamentResponse
+    {
+        private DelegateCommand _selectTournamentCommand;
+        private readonly INavigationService _navigationService;
+
+        public DelegateCommand SelectTournamentCommand => _selectTournamentCommand ?? (_selectTournamentCommand = new DelegateCommand(SelectTournamentAsync));
+        // When make "tap" execute method SelectTournament
+
+        public TournamentItemViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+        private async void SelectTournamentAsync()
+        {
+            var parameters = new NavigationParameters
+            {
+                { "tournament", this }
+            };
+            await _navigationService.NavigateAsync(nameof(GroupsPage), parameters);
+        }
+    }
+}
